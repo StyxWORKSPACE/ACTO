a60fec50965a9a1d560457e7f48b25fb2e4b303c
jinu <workspace_@naver.com>
2025-02-12 19:06:37 +0900

    Update eidt circle_progress_indicator

--- lib/data/services/github_service.dart
+++ lib/data/services/github_service.dart
@@ -1,7 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../models/github_models.dart';
-import '../../core/config/app_config.dart';
 
 class GitHubService {
   final String token;
